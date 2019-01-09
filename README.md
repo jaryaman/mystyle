b1ef3f53ddb259d15ff7eb9451c896a3fcd555c3
jaryaman <j.aryaman25@gmail.com>
2019-01-09 16:52:04 +0000

    Give installation instructions

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 A set of functions to tweak matplotlib default plotting style and some useful plots
 
 Author: Juvid Aryaman
+
+## Installation
+
+```
+$ git clone https://github.com/jaryaman/mystyle/.git
+$ cd mystyle
+$ python setup.py sdist
+$ pip install ./dist/mystyle-0.0.1.tar.gz
+```
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='mystyle',
+	version='0.0.1',
+	description='A set of functions to tweak matplotlib default plotting style and some common useful plots',
+	licence='MIT',
+	url='https://github.com/jaryaman/mystyle/',
+	author='Juvid Aryaman',
+	author_email='j.aryaman25@gmail.com',
+	keywords=['matplotlib'],
+	packages=['mystyle'],
+	#install_requires=['numpy','scipy','matplotlib','os','time','pandas','os','warnings','functools','ipywidgets'],
+)
