c605d1726370536aefbae6eeda40092471bdf09d
Juvid Aryaman <j.aryaman25@gmail.com>
2019-02-10 17:54:20 +0000

    Add to do to calc_MI

--- ana.py
+++ ana.py
@@ -75,6 +75,11 @@ def calc_MI(x, y, bins):
 	--------------
 	https://stackoverflow.com/questions/20491028/optimal-way-to-compute-pairwise-mutual-information-using-numpy
 	https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mutual_info_score.html
+
+	To do
+	--------------
+	- Find a rational way of picking bins
+	- Read up about this some more
 	"""
     c_xy = np.histogram2d(x, y, bins)[0]
     mi = mutual_info_score(None, None, contingency=c_xy)
